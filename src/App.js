5d9114a10f08a97d20b6e74aa223875f5dd8418e
Nicole Portalatin <nportalatin@xpanxion.com>
2023-08-13 21:49:38 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import NavigationBar from './components/NavigationBar'; // Add this line
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import AboutMe from './components/AboutMe';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar /> {/* Add the navigation bar */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path='/about-me' element={<AboutMe/>}/>
+          {/* Add more routes for other sections */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
